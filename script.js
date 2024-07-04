56c796843bc8c96db80ba8bdfbe23b1dd4c3d39d
SHonour <97793725+SHonour@users.noreply.github.com>
2024-07-04 15:22:25 +0100

    script.js

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 function UpdateTime () {
-const CurrentTime = document.getElementById('currentTimeinUTC');
-const now = new Date();
-CurrentTime.textContent = now.toUTCString();
-}
-
-
-function UpdateDay() {
-  const CurrentDay = document.getElementById('CurrentDay');
+  const CurrentTime = document.getElementById('currentTimeUTC');
   const now = new Date();
-  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  CurrentDay.textContent = days[now.getUTCDay()];
+  CurrentTime.textContent = now.toUTCString();
   }
-
   
-UpdateDay();
-UpdateTime();
-
-setInterval(UpdateTime, 1000);
+  
+  function UpdateDay() {
+    const CurrentDay = document.getElementById('currentDay');
+    const now = new Date();
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    CurrentDay.textContent = days[now.getUTCDay()];
+    }
+  
+    
+  UpdateDay();
+  UpdateTime();
+  
+  setInterval(UpdateTime, 1000);
+  
 
 
 
